be9513931c5523203051aa24b25dd598a753abb9
Peleg <45825608+pkd2210@users.noreply.github.com>
2026-07-04 20:13:43 +0300

    update education page

--- src/pages/Education.jsx
+++ src/pages/Education.jsx
@@ -97,9 +97,20 @@ function FaqItem({ q, a }) {
 export default function Education() {
   return (
     <section className="eduPage almoni">
+      <div className="eduTopImages">
+        <div className="eduTopImageLarge">
+          <img style={{ width: "100%", height: "100%", objectFit: "cover" }} src={`https://fastly.picsum.photos/id/336/1000/600.jpg?hmac=lT6Ac0boYjztwD03xvbfl-ekNYSXh-d78c0XX9s9YAE`} draggable={false} alt="תוכנית מערכת החינוך" />
+        </div>
+        <div className="eduTopImageSmall">
+          <img style={{ width: "100%", height: "100%", objectFit: "cover" }} src={`https://fastly.picsum.photos/id/17/1000/200.jpg?hmac=NN8eOku060bz_6uZiti477rN0VXwGMr3aMwTPpaCV2I`} draggable={false} alt="תוכנית מערכת החינוך" />
+        </div>
+      </div>
+
       <div className="eduHero">
         <h1 className="brand-blue">התוכנית למערכת החינוך</h1>
         <p className="subtitle">חינוך איכותי ושוויוני לכל ילד וילדה בישראל</p>
+        <hr style={{ border: "none", borderTop: "1px solid rgb(0, 59, 137)", margin: "20px 0" }}></hr>
+        <br></br>
       </div>
 
       <div className="eduSection eduProblemSolutionGrid">
@@ -135,6 +146,19 @@ export default function Education() {
         <div className="eduGoalsGrid">
           {goals.map((g, i) => (
             <div className="eduGoalCard" key={i}>
+              <h3>{g.title}</h3>
+              <p>{g.desc}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="eduSection eduSuccessSection">
+        <h2 className="eduGoalsTitle">איך נדע שהצלחנו</h2>
+        <div className="eduGoalsList">
+          {goals.map((g, i) => (
+            <div className="eduGoalItem" key={i}>
+              <i className="fa-solid fa-check eduGoalCheck"></i>
               <h3>{g.title}</h3>
               <p>{g.desc}</p>
             </div>
