05b02fbc98af61f9d99f7b2c176bf6166ff3b1c9
Tommy Danieli <tomdanieli2009@gmail.com>
2026-07-04 19:17:48 +0300

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Layout from "./Layout";
 import Home from "./pages/Home";
 import AboutMe from "./pages/AboutMe";
+import Plans from "./pages/Plans";
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <Route element={<Layout />}>
         <Route path="/" element={<Home />} />
         <Route path="/aboutme" element={<AboutMe />} />
+        <Route path="/plans" element={<Plans />} />
       </Route>
     </Routes>
   );
