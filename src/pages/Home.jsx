39cbb85edcc3ff3b36a7fdb86e2b8e8d1d84429c
Peleg <45825608+pkd2210@users.noreply.github.com>
2026-07-09 14:06:51 +0300

    Update hero

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -90,7 +90,7 @@ export default function Home() {
     <>
       <section className="hero">
         <img
-          src="/images/hero-banner.png"
+          src="/images/new hero.png"
           draggable={false}
           alt="תומי דניאלי - מיישרים קו"
           fetchPriority="high"
